5a09c49cc42f4d622c8d397089653f622dcfaaef
SnowCharm <619022098@qq.com>
2022-09-26 20:09:34 +0800

    add practice3.py

---
+++ lab4/practice3.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+
+class Echo(threading.Thread):
+    def __init__(self, conn, address):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.address = address
+
+    def run(self) -> None:
+        while True:
+            data = self.conn.recv(2048)
+            if data and data != b'exit\r\n':
+                self.conn.send(data)
+                print('{} sent: {}'.format(self.address, data))
+            else:
+                self.conn.close()
+                return
+
+
+def echo():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('127.0.0.1', 5555))
+    s.listen(10)
+    while True:
+        conn, address = s.accept()
+        Echo(conn, address).start()
+
+
+if __name__ == "__main__":
+    try:
+        echo()
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
