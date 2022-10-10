1545143619986663ff4534ecf5d27ab1a79796d4
SnowCharm <619022098@qq.com>
2022-10-11 00:06:01 +0800

    add lab5

---
+++ lab5/client.py
@@ -0,0 +1,11 @@
+from socket import *
+
+server_name = "127.0.0.1"
+server_port = 12000
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+while True:
+    message = input("Input the qname and rdtype:")
+    client_socket.sendto(message.encode(), (server_name, server_port))
+    rec_msg, server_addr = client_socket.recvfrom(2048)
+    print(rec_msg)
---
+++ lab5/server.py
@@ -0,0 +1,19 @@
+import dns.resolver
+from socket import *
+
+server_port = 12000
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind(('127.0.0.1', server_port))
+print("The server is ready to receive")
+
+while True:
+    message, client_addr = server_socket.recvfrom(2048)
+    modified_message = message.decode()
+    qname, rdtype = modified_message.split(" ")
+    ans = dns.resolver.resolve(qname=qname, rdtype=rdtype, raise_on_no_answer=False)
+    msg = ""
+    for i in ans.response.answer:
+        for j in i.items:
+            msg += str(j)
+            msg += " "
+    server_socket.sendto(msg.encode(), client_addr)
